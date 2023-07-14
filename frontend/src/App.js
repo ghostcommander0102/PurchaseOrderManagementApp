17116383ae8adc62e649b385f64232de214635ee
ghostcommander0102 <ghostcommander0102@outlook.com>
2023-07-14 17:28:59 -0600

    implement request to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,34 @@ function App() {
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
+
+		const formData = new FormData();
+		formData.append('file', file);
+		formData.append('date', date);
+		formData.append('vendorName', vendorName);
+
+		fetch('http://localhost:5000/api/purchase-order', {
+			method: 'POST',
+			body: formData,
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.success) {
+					// Purchase order submitted successfully
+					setErrorMessage('');
+					// Reset form fields
+					setFile(null);
+					setDate('');
+					setVendorName('');
+				} else {
+					// Handle validation errors
+					setErrorMessage(data.error);
+				}
+			})
+			.catch((error) => {
+				console.error('Error submitting purchase order:', error);
+				setErrorMessage('Failed to submit purchase order');
+			});
 	};
 
 	return (
