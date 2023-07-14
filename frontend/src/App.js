2b6429e5383d8b6e1ce71b3599bbfac2c2ff69e0
ghostcommander0102 <ghostcommander0102@outlook.com>
2023-07-14 17:25:29 -0600

    interface integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [file, setFile] = useState(null);
+	const [date, setDate] = useState('');
+	const [vendorName, setVendorName] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const handleFileChange = (event) => {
+		setFile(event.target.files[0]);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<div className="App">
+			<h1>Purchase Order Form</h1>
+			{errorMessage && <p className="error">{errorMessage}</p>}
+			<form onSubmit={handleSubmit}>
+				<label>Date:</label>
+				<input type="text" value={date} onChange={(e) => setDate(e.target.value)} />
+				<br />
+				<label>Vendor Name:</label>
+				<input type="text" value={vendorName} onChange={(e) => setVendorName(e.target.value)} />
+				<br />
+				<label>CSV File:</label>
+				<input type="file" onChange={handleFileChange} />
+				<br />
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
