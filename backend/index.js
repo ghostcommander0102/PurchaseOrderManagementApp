d889e36498ed781023fadfa899ca577fcfd2edb7
ghostcommander0102 <ghostcommander0102@outlook.com>
2023-07-14 17:59:34 -0600

    integrate api

--- backend/index.js
+++ backend/index.js
@@ -2,46 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const csvParser = require('csv-parser');
-const express = require('express');
-const app = express();
 const routes = require('./routes');
 
+const app = express();
 const upload = multer();
-
-// DB connect
-db.connect();
 
 // Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 
-// Register the routes
-app.use('/api', routes);
+// Parse request body
+app.use(express.json());
 
-
-// Endpoint for processing the purchase order
-app.post('/api/purchase-order', upload.single('file'), (req, res) => {
-    const { date, vendorName } = req.body;
-    const purchaseOrderItems = [];
-
-    // Parse the CSV file
-    csvParser()
-        .on('data', (data) => {
-            const { modelNumber, unitPrice, quantity } = data;
-            purchaseOrderItems.push({ modelNumber, unitPrice, quantity });
-        })
-        .on('end', () => {
-            // Perform validation and store data in your preferred storage
-
-            // Return a response indicating success or errors
-            res.json({ success: true });
-        })
-        .on('error', (error) => {
-            console.error('Error parsing CSV:', error);
-            res.status(400).json({ success: false, error: 'Failed to parse CSV file' });
-        });
-
-    req.file.pipe(csvParser());
-});
+// Mount routes
+app.use('/', routes);
 
 // Start the server
 app.listen(5000, () => {
--- backend/models/purchaseorder.js
+++ backend/models/purchaseorder.js
@@ -1,27 +1,43 @@
-'use strict';
-const {
-    Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-    class PurchaseOrder extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
+const { PurchaseOrder } = require('../models');
+const csvParser = require('csv-parser');
+
+const purchaseOrderController = {
+    create: async (req, res) => {
+        try {
+            const { date, vendorName } = req.body;
+            const purchaseOrderItems = [];
+
+            // Parse the uploaded CSV file
+            req.pipe(req.busboy);
+            req.busboy.on('file', (fieldname, file, filename) => {
+                file.pipe(csvParser())
+                    .on('data', (data) => {
+                        // Process each row of the CSV file
+                        const { modelNumber, unitPrice, quantity } = data;
+
+                        // Create a purchase order item
+                        const purchaseOrderItem = {
+                            modelNumber,
+                            unitPrice: parseFloat(unitPrice),
+                            quantity: parseInt(quantity),
+                            date: new Date(date),
+                            vendorName,
+                        };
+
+                        purchaseOrderItems.push(purchaseOrderItem);
+                    })
+                    .on('end', async () => {
+                        // Insert the purchase order items into the database
+                        await PurchaseOrder.bulkCreate(purchaseOrderItems);
+
+                        res.json({ success: true });
+                    });
+            });
+        } catch (error) {
+            console.error('Error saving purchase order:', error);
+            res.status(500).json({ success: false, error: 'Failed to save purchase order' });
         }
-    }
-    PurchaseOrder.init({
-        modelNumber: DataTypes.STRING,
-        unitPrice: DataTypes.DECIMAL,
-        quantity: DataTypes.INTEGER,
-        date: DataTypes.DATE,
-        vendorName: DataTypes.STRING
-    }, {
-        sequelize,
-        modelName: 'PurchaseOrder',
-    });
-    return PurchaseOrder;
+    },
 };
+
+module.exports = purchaseOrderController;
